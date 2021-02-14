32b5d0446c837508df23457407e317064d3c597c
ShukrithiRathna <shukrithi@gmail.com>
2021-02-14 18:41:08 +0530

    Added files for basic set up

---
+++ .bash_aliases
@@ -0,0 +1,15 @@
+
+#ALIAS:
+#cd 
+alias ..='cd ../'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias .....='cd ../../../../'
+alias update='sudo apt update;sudo apt upgrade'
+alias home='cd ~'
+alias gh='history|grep'
+#open file
+alias open='xdg-open'
+alias home='cd ~/'
+alias pve= 'python3 -m venv'
+
---
+++ scripts/symlink.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+create_symlink () {
+  if [ -h ~/${1} ]; then
+    # Existing symlink 
+    echo "Removing existing symlink: ${dest}"
+    dest="${HOME}/${1}"
+    rm ${dest} 
+ fi
+  ln -sv ~/dotfiles/${1} ~   
+}
+
+create_symlink .bashrc 
+create_symlink .bash_aliases 
+create_symlink .gitconfig 
